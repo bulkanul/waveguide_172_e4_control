a2e9c5535349ce3398f3ae985c73d0d3dfd90fa6
bulkanul <anton.segnet@gmail.com>
2025-08-19 13:05:25 +0200

    protocol version fixed

--- Core/Inc/gitcommit.h
+++ Core/Inc/gitcommit.h
@@ -1,2 +1,2 @@
 #define GIT_INFO_PRESENT
- static const char* GIT_INFO = "Version Information=[1f66163dcc626b03f7dd5c8a0fbc5fc93f65d6cf, (HEAD -> master, origin/master, origin/HEAD)]\r\n";
+ static const char* GIT_INFO = "Version Information=[a5ad0df53b0c402e5695049f126102073aa93e49, (HEAD -> master, origin/master, origin/HEAD)]\r\n";
